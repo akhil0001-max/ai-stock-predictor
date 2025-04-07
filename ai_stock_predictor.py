475b34468b0a504edf00cb6832f3c53d9a140230
akhil0001-max <akhilendravlog69@gmail.com>
2025-04-07 11:41:33 +0530

    Update ai_stock_predictor.py

--- ai_stock_predictor.py
+++ ai_stock_predictor.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import streamlit as st 
 from sklearn.preprocessing import MinMaxScaler 
 from tensorflow.keras.models import Sequential 
-from tensorflow.keras.layers import LSTM, 
+from tensorflow.keras.layers import LSTM 
 Dense, Dropout import datetime
 
 st.set_page_config(page_title="AI Stock Predictor", layout="centered") st.title("AI-based Stock Predictor (Deep Learning)")
